3fde752375df4bea1ed7c3093d41f1859a80ad83
s.antosenkov <s.antosenkov@iva.ru>
2025-03-05 16:44:21 +0300

    qweq

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
         return
 
     # Выводим исходный список
-    print("Исходнfый список:", numbers)
+    print("Исходный список:", numbers)
 
     # Сортируем методом пузырька
     bubble_sort(numbers)
